bf02dc8f573887443879b56df34c576fd347fa46
tarosuke <felis.silvestris.catus.7777@gmail.com>
2020-04-07 12:58:06 +0900

    init

---
+++ pages/Product.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>{{ title }}</h2>
+</template>
+
+<script>
+export default {
+  async asyncData (context) {
+    return await new Promise(resolve => {
+      // バックエンドからのデータ取得をエミュレート
+      setTimeout(() => {
+        resolve({
+          title: `このページはNuxt社のプロダクトページです。(${context.isServer ? 'サーバサイドレンダリング' : 'クライアントサイドレンダリング'})`
+        })
+      }, 1000)
+    })
+  } 
+}
+</script>
